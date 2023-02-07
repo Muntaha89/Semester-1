17b0807c2afc424a41acc5cbe19cd5c2e70c2d9f
Salman <101444405+salmannaeem112215@users.noreply.github.com>
2023-02-07 05:07:38 -0800

    Add Comments and Vallidate User Entry

--- Pf/task2/task2/Source.cpp
+++ Pf/task2/task2/Source.cpp
@@ -3,10 +3,17 @@ using namespace std;
 int main()
 {
 	int num,result;
-	cout << " Display the integer ";
+	
+	// This loop will validate user to enter positive num
+	do{
+	cout << " Enter integer => ";
 	cin >> num;
+	}while(num<0);
+	
+	// Iteration Loop till User Entered Num
 	for (int x = 0;x <= num;x++)
 	{ 
+		// Printing Square of a number
 		result = x * x;
 		cout << " Square of " << x << " is " << result << endl;
 
